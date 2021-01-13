a54af7d29bdc1a9f09929dca18abe9fa81326288
bente <marcus.bentele@web.de>
2021-01-13 17:56:50 +0100

    Application change; BPMN Anpassung

---
+++ src/main/java/at/fhv/ProjectApprovalApplication.java
@@ -0,0 +1,37 @@
+package at.fhv;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class ProjectApprovalApplication {
+
+  public static void main(String... args) {
+    // Avoid resetting URL stream handler factory
+    TomcatURLStreamHandlerFactory.disable();
+    SpringApplication.run(ProjectApprovalApplication.class, args);
+  }
+
+  @Autowired
+  protected ProcessEngine processEngine;
+
+  @PostConstruct
+  public void deployInvoice() {
+    ClassLoader classLoader = ProjectApprovalApplication.class.getClassLoader();
+
+    if(processEngine.getIdentityService().createUserQuery().list().isEmpty()) {
+      processEngine.getRepositoryService()
+          .createDeployment()
+          .addInputStream("diagram_1.bpmn", classLoader.getResourceAsStream("diagram_1.bpmn"))
+          .deploy();
+    }
+  }
+}
+
